1e3f0d2fe54c51abd13b8b55e87a5fbe3dd21349
wikki <muthumanikandanapk@gmail.com>
2024-06-30 23:40:06 +0530

    Arithmetic Operators - Python

---
+++ Python/Day-02/02 - Arithmetic Operators.py
@@ -0,0 +1,18 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/python-arithmetic-operator
+# Difficulty: Easy
+# Max Score: 10
+# Language: Python
+
+# ========================
+#         Solution
+# ========================
+
+a = int(input())
+b = int(input())
+print(a + b)
+print(a - b)
+print(a * b)
