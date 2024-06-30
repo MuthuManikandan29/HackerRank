0a68b8a2ca7932d893c6902e8510024f64a1e0c6
wikki <muthumanikandanapk@gmail.com>
2024-06-30 23:59:54 +0530

    Python - Divison

---
+++ Python/Day-02/03 - Division.py
@@ -0,0 +1,17 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/python-division
+# Difficulty: Easy
+# Max Score: 10
+# Language: Python
+
+# ========================
+#         Solution
+# ========================
+
+a = int(input())
+b = int(input())
+print(a//b)
+print(a/b)
