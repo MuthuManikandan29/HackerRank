fabeb637ff90426a18f82b0cf0243dbc7884a3dd
wikki <muthumanikandanapk@gmail.com>
2024-07-14 23:28:43 +0530

    Refactor 30 Days of Code Python Solutions

---
+++ 30 Days of Code/Python/01 - Day 0 - Hello, World.py
@@ -0,0 +1,21 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/30-hello-world/problem
+# Difficulty: Easy
+# Max Score: 30
+# Language: Python
+
+# ========================
+#         Solution
+# ========================
+
+# get a line of input from stdin and save it to our variable
+inputString = input()
+
+# Your first line of output goes here
+print('Hello, World.')
+
+# Write the second line of output
+print(inputString)
